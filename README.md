6cee00830d8c037787e522363aecd13175f0af22
Robertarezza <roberta.rezza@gmail.com>
2024-03-27 19:01:22 +0100

    add: js

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
  **BONUS 2:**
  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
  Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
+ Svolgimento
+ 1. stamapre in console numeri da 1 a 100
+ 2. stampa in console i multipli di 3 con scritta "Fizz", i multipli di 5 con scritta "Buzz" e per i multipli di 3 e 5 la scritta "Fizz Bazz"
+ 3. creo un ciclo per la stampa dei numeri da 1 a 100.
+ 4. creo una variabile per il testo in base ai se
---
+++ js/script.js
@@ -0,0 +1,37 @@
+//test
+//alert("ciao")
+
+const rowElem = document.querySelector(".row"); // object | null
+console.log(rowElem);
+
+
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    
+    let redClass= document.querySelector(".red");
+    let text; 
+        if ( i % 3 === 0 && i % 5 === 0) {
+           
+            text = "Fizz";
+            
+         } else if ( i % 5 === 0){
+            text = redClass;
+          
+            } else if ( i % 3 === 0) {
+                text = "Fizz Buzz";
+            } else  {
+                text = i;
+            }
+            
+            
+            const boxElem = document.createElement("div"); // object
+            boxElem.innerHTML = text;
+            boxElem.classList.add( "box" );
+            
+            console.log(boxElem);
+            rowElem.append(boxElem);
+        
+             
+
+        }
